946f07ab1273c0068767f9cada84bd060afdf2af
Daniyar <danikman89@gmail.com>
2023-09-08 18:31:57 +0600

    refactor: change structure

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,11 +1,32 @@
 <template>
-  <div v-for="post in posts" class="post">
-    <div><strong>Название:</strong> {{ post.title }}</div>
-    <div><strong>Описание:</strong> {{ post.body }}</div>
+  <div class="post">
+    <div>
+      <div><strong>Название:</strong> {{ post.title }}</div>
+      <div><strong>Описание:</strong> {{ post.body }}</div>
+    </div>
+    <div class="btns">
+      <button>Удалить</button>
+    </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.post {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+</style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
-  <div v-for="post in posts" class="post">
-    <div><strong>Название:</strong> {{ post.title }}</div>
-    <div><strong>Описание:</strong> {{ post.body }}</div>
+  <div>
+    <h3>Список постов</h3>
+    <post-item v-for="post in posts" :post="post" />
   </div>
 </template>
 
 <script>
+import PostItem from './PostItem.vue';
 export default {
+  components: { PostItem },
   props: {
     posts: {
       type: Array,
@@ -17,10 +19,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.post {
-  padding: 15px;
-  border: 2px solid teal;
-  margin-top: 15px;
-}
-</style>
+<style scoped></style>

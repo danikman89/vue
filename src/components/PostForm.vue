5e51f4ff8e261ee3cd96fafe74970d84c07a789b
Daniyar <danikman89@gmail.com>
2023-09-08 12:36:00 +0600

    feat: add PostForm component

---
+++ src/components/PostForm.vue
@@ -0,0 +1,53 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input
+      v-bind:value="title"
+      @input="inputValue"
+      class="input"
+      type="text"
+      placeholder="Название"
+    />
+    <input
+      v-bind:value="body"
+      @input="body = $event.target.value"
+      class="input"
+      type="text"
+      placeholder="Описание"
+    />
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  background: none;
+  padding: 10px 20px;
+  color: teal;
+  align-self: flex-end;
+  border: 1px solid teal;
+  margin-top: 15px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  color: white;
+  background-color: teal;
+}
+</style>
